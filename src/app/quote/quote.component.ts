8c3e2ab301f95a64a26fa8aebc82e2bb59c827b5
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-08 21:32:33 +0300

    Feat: Adds logic - This adds the logic to the quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quote: Quote[] = [
+    new Quote(1, "Abraham Lincoln", "I don't like that man. I must get to know him better", 0, 0),
+    new Quote(2, "Scottie Pippen", "Once you get a taste of success, it's a hard thing to turn down", 0, 0),
+    new Quote(3, "Nadia Comaneci", "Enjoy the journey and try to get better every day. And don't lose the passion and the love for what you do", 0, 0),
+    new Quote(4, "Albert Einstein", "Logic will get you from A to B. Imagination will take you everywhere", 0, 0),
+  ] 
 
   constructor() { }
 
