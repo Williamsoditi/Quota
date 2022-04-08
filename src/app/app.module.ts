d6a3e1bc3520df8f03bd64d6cb922914cef08a25
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-08 20:43:04 +0300

    Feat : Add quote component - This adds the quote component with no logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    QuoteComponent,
   ],
   imports: [
     BrowserModule,
