1e9bccf650e791b7bdb58b10bf49ee47c004d238
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-09 10:36:52 +0300

    Feat: Real time typing - Real time typing optimised on the author name field

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0, "","",0,0, new Date());
   constructor() { }
 
   ngOnInit(): void {
