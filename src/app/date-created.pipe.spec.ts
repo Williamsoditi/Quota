bae3bebee2e6fc991f9ac7cef0ac43504698bb76
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-10 16:58:05 +0300

    Auto-generated commit [ci skip]

--- src/app/date-created.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateCreatedPipe } from './date-created.pipe';
-
-describe('DateCreatedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateCreatedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    showAuthor: boolean;
-    constructor(public id: number, public author: string, public quote: string, public upvote: number, public downvote: number, public completeDate: Date){
-        this.showAuthor = false;
-    }
-}
